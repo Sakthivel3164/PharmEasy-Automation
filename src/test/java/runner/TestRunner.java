81d2cf0be16dafa444ee5e10210e05d9ef7e9e60
sakthivijayan2 <sakthivijayan2@gmail.com>
2024-04-24 17:06:16 +0530

    xl file added

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,7 +2,8 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-@CucumberOptions( features = { "classpath:features/" }, glue = { "classpath:step_definition" })
+
+@CucumberOptions(features = { "classpath:features/" }, glue = { "classpath:step_definition" })
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
