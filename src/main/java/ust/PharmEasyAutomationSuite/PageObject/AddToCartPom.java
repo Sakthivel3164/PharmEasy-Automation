50b0e244724f489687a2fa7df7c142730d6a636d
shebin <shebin2244@gmail.com>
2024-04-29 22:39:49 +0530

    changes in addtocart

--- src/main/java/ust/PharmEasyAutomationSuite/PageObject/AddToCartPom.java
+++ src/main/java/ust/PharmEasyAutomationSuite/PageObject/AddToCartPom.java
@@ -1,5 +1,7 @@
 package ust.PharmEasyAutomationSuite.PageObject;
 
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,7 +20,9 @@ public class AddToCartPom extends DynamicLocaters{
 	@FindBy(className = "Search_searchHeading__DWX9l")
 	WebElement resultTitleElement;
 	@FindBy(css = "div[class='LHS_container__mrQkM Search_fullWidthLHS__mteti']>div:nth-child(2)>div>div>a>div:nth-child(2)>div>div:nth-child(4)>div>div:last-child>div>div>div>div>button")
-	WebElement addtoCartbuttonElement;
+	WebElement addtoCartbuttonElement1;
+	@FindBy(xpath = "//div[@class='MuiInputBase-root MuiInput-root QuantitySelector_selectStyle__Ts3i3 MuiInputBase-formControl MuiInput-formControl']")
+	WebElement addtoCartbuttonElement2;
 
 	@FindBy(className = "Navigation_notificationCountBubble__G4f_P")
 	WebElement cartcountElement;
@@ -35,11 +39,23 @@ public class AddToCartPom extends DynamicLocaters{
 		return rf.ReturnGetText(resultTitleElement);
 	}
 
-	public String addMed(String string) {
-		rf.clickOnElement(addtoCartbuttonElement);
-		rf.clickOnElement(addCountElement(driver, string));
-		return rf.ReturnGetText(cartcountElement);//returning cart count eg.3
-	}
+	 public WebElement getDisplayedElement() {
+	        if (addtoCartbuttonElement1.isDisplayed()) {
+	            return addtoCartbuttonElement1;
+	        } else if (addtoCartbuttonElement2.isDisplayed()) {
+	            return addtoCartbuttonElement2;
+	        } else {
+	            throw new NoSuchElementException("No add to cart button is displayed.");
+	        }
+	    }
+
+	    public String addMed( String string) {
+	        WebElement addtoCartButton = getDisplayedElement();
+	        rf.clickOnElement(addtoCartButton);
+	        rf.clickOnElement(addCountElement(driver, string));
+	        return rf.ReturnGetText(cartcountElement); //returning cart count eg.3
+	    }
+	
 
 	
 }
